cdb0c1bc5b0ce62a9601f8954cc9cc4e7a39eb16
Nezih Öztürk <nzhzturk@hotmail.com>
2020-05-26 10:41:14 +0200

    Core

---
+++ Zen.Cqrs.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zen.Cqrs.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
